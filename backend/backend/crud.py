b4768b409725589a0958c322ab23e67377dfdd07
Wellington Joji Endo <wellington.endo@edu.unifil.br>
2021-11-25 15:10:57 -0300

    finalizado, porém com alguns problemas!

--- backend/backend/crud.py
+++ backend/backend/crud.py
@@ -1,3 +1,4 @@
+from pydantic.errors import SetError
 from sqlalchemy.orm import Session
 from . import models, schemas
 
@@ -18,6 +19,25 @@ def create_user(db: Session, user: schemas.UserCreate):
         hashed_password=fake_hashed_password
     )
     db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def edit_user(db: Session, user_id: int, new_value: schemas.UserEdit):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first() # get user by id
+    if db_user: # if id exist
+        if new_value.name != None: # Change if not null
+            db.query(models.User).filter(models.User.id == user_id).\
+                update({"name": new_value.name})
+
+        if new_value.email != None: # Change if not null
+            db.query(models.User).filter(models.User.id == user_id).\
+                update({"email": new_value.email})
+
+        if new_value.birth_date != None: # Change if not null
+            db.query(models.User).filter(models.User.id == user_id).\
+                update({"birth_date": new_value.birth_date})
+
     db.commit()
     db.refresh(db_user)
     return db_user
